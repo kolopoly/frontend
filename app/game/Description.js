0e687288de3083fb91166fd4ba3142b6118872e5
MikalaiNavitski <mikalai.navitski@zendesk.com>
2024-04-22 14:54:34 +0200

    description

--- app/game/Description.js
+++ app/game/Description.js
@@ -20,13 +20,13 @@ class Description extends React.Component {
                 <div><span>Sell: <span>{sellPrice}</span>$</span></div>
                 <div><span>Upgrade: <span>{upgradePrice}</span>$</span></div>
                 <div>Fees:</div>
-                <div>1. <span>{fees[0]}</span>$ <span>{fieldLevel === 1 ? '*' : ''}</span></div>
+                <div>1. <span>{fees[0]}</span>$ <span>{fieldLevel === 0 ? '*' : ''}</span></div>
                 <div>2. <span>{fees[1]}</span>$ <span>{fieldLevel === 1 ? '*' : ''}</span></div>
-                <div>3. <span>{fees[2]}</span>$ <span>{fieldLevel === 1 ? '*' : ''}</span></div>
-                <div>4. <span>{fees[3]}</span>$ <span>{fieldLevel === 1 ? '*' : ''}</span></div>
-                <div>5. <span>{fees[4]}</span>$ <span>{fieldLevel === 1 ? '*' : ''}</span></div>
-                <div>6. <span>{fees[5]}</span>$ <span>{fieldLevel === 1 ? '*' : ''}</span></div>
-                <div>7. <span>{fees[6]}</span>$ <span>{fieldLevel === 1 ? '*' : ''}</span></div>
+                <div>3. <span>{fees[2]}</span>$ <span>{fieldLevel === 2 ? '*' : ''}</span></div>
+                <div>4. <span>{fees[3]}</span>$ <span>{fieldLevel === 3 ? '*' : ''}</span></div>
+                <div>5. <span>{fees[4]}</span>$ <span>{fieldLevel === 4 ? '*' : ''}</span></div>
+                <div>6. <span>{fees[5]}</span>$ <span>{fieldLevel === 5 ? '*' : ''}</span></div>
+                <div>7. <span>{fees[6]}</span>$ <span>{fieldLevel === 6 ? '*' : ''}</span></div>
             </div>
         );
     }
