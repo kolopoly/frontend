3f960cf3f02b8b54562627126964d8bb93ca5ec9
l0gun0v <98526047+l0gun0v@users.noreply.github.com>
2024-06-03 05:58:55 +0200

    backend

--- app/backend.js
+++ app/backend.js
@@ -1,5 +1,5 @@
-const backend = "https://my-image-me3ntghbrq-uk.a.run.app"
-const wsbackend = "ws://my-image-me3ntghbrq-uk.a.run.app"
+export const backend = "https://my-image-me3ntghbrq-uk.a.run.app"
+export const wsbackend = "ws://my-image-me3ntghbrq-uk.a.run.app"
 
 //export const backend = "http://localhost:8000"
 //export const wsbackend = "ws://localhost:8000"
