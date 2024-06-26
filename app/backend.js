aa3a96ec9def352e2a8f5aca938d2b31bae5154c
Andrei Mishchanka <52292591+AndreiMishchanka@users.noreply.github.com>
2024-06-26 16:29:53 +0200

    Update backend.js

--- app/backend.js
+++ app/backend.js
@@ -1,5 +1,5 @@
 export const backend = "https://my-image-me3ntghbrq-uk.a.run.app"
-export const wsbackend = "ws://my-image-me3ntghbrq-uk.a.run.app"
+export const wsbackend = "wss://my-image-me3ntghbrq-uk.a.run.app"
 
 //export const backend = "http://localhost:8000"
 //export const wsbackend = "ws://localhost:8000"
